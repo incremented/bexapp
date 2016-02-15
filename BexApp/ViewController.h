86679b19971357a42d13b15c92767d4cd9ed7437
Timotheus Jochum <timo.jochum@gmx.net>
2016-02-15 17:46:59 +0100

    Initial Commit

---
+++ BexApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BexApp
+//
+//  Created by Timotheus Jochum on 15/02/16.
+//  Copyright © 2016 Timotheus Jochum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BexApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BexApp
+//
+//  Created by Timotheus Jochum on 15/02/16.
+//  Copyright © 2016 Timotheus Jochum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
