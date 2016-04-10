906befda185805a9d11f9fae94a36c5fa16c23c2
Timotheus Jochum <timo.jochum@gmx.net>
2016-04-10 19:58:21 +0200

    New Galery

--- KanzleiViewController.h
+++ KanzleiViewController.h
@@ -8,10 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-@interface KanzleiViewController : UIViewController <UIGestureRecognizerDelegate>
+@interface KanzleiViewController : UIViewController <UIGestureRecognizerDelegate, UICollectionViewDataSource, UICollectionViewDelegate>
 
 
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *sidebarButton;
+@property (nonatomic, weak) IBOutlet UICollectionView *collectionView;
 @property (strong, nonatomic) IBOutlet UIImageView *imageView;
 @property (strong, nonatomic) IBOutlet UIPageControl *pageControl;
 @property (strong, nonatomic) NSArray *images;
